d0d6568ae49ee26870f6980bedd38052e22ee21d
lucapelizzari <117814075+lucapelizzari@users.noreply.github.com>
2025-05-23 11:52:57 +0200

    Add files via upload

--- Non linear signature optimal stopping/Deep_signatures_optimal_stopping.py
+++ Non linear signature optimal stopping/Deep_signatures_optimal_stopping.py
@@ -8,12 +8,16 @@ This file contains three ways of computing lower and upper bounds to the optimal
 deep neural networks on signatures, and using the signature kernel.
 
 """
+
+import shap
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras import layers, models, optimizers, regularizers
 from functools import reduce
-
+from tensorflow.keras.layers import Input, Layer, Multiply
+import matplotlib.pyplot as plt
+import timeshap
 class DeepLongstaffSchwartzPricer:
     """
     Computes the lower bound of optimal stopping problem using deep neural networks on signatures.
@@ -80,7 +84,7 @@ class DeepLongstaffSchwartzPricer:
         M_val : int
             Number of validation paths
         """
-        
+	
         M, N, feature_dim = S_training_sig.shape
         N= N-1
         M2, _, _ = S_testing_sig.shape
@@ -114,7 +118,7 @@ class DeepLongstaffSchwartzPricer:
                 regr[k-1] = LongstaffSchwartzModel(
                     feature_dim=feature_dim,
                     layers_number=self.layers,
-                    nodes=self.nodes,
+                    nodes=self.nodes+feature_dim,
                     activation_function=self.activation_function,
                     batch_normalization=self.batch_normalization,
                     regularizer=self.regularizer,
@@ -137,7 +141,7 @@ class DeepLongstaffSchwartzPricer:
                         print("Continuing with randomly initialized weights")
                 
                 print(f"Regression at exercise date {k}")
-                early_stopping = EarlyStopping(monitor='loss', patience=5)
+                early_stopping = EarlyStopping(monitor='loss', patience=5,restore_best_weights=True)
                 if M_val == 0:
                     regr[k-1].fit(
                     S_exercise_training_new,
@@ -185,7 +189,7 @@ class DeepLongstaffSchwartzPricer:
                 regr[j-1] = LongstaffSchwartzModel(
                     feature_dim=feature_dim,
                     layers_number=self.layers,
-                    nodes=self.nodes,
+                    nodes=self.nodes+feature_dim,
                     activation_function=self.activation_function,
                     batch_normalization=self.batch_normalization,
                     regularizer=self.regularizer,
@@ -249,7 +253,7 @@ class DeepLongstaffSchwartzPricer:
                 i = 0
                 while i < self.N1 - 1 and reg[m, i] > Payoff_exercise_testing[m, i]:
                     i += 1
-                value_testing[m] = Payoff_exercise_testing[m, i] * np.exp(-self.r * self.T * (ttt[i+1] - ttt[1]))
+                value_testing[m] = Payoff_exercise_testing[m, i] * np.exp(-self.r * self.T * (ttt[i+1] - ttt[0]))
         
         elif self.mode == "American Option":
             """
@@ -262,9 +266,10 @@ class DeepLongstaffSchwartzPricer:
                 value_testing[m] = Payoff_exercise_testing[m, i] * np.exp(-self.r * self.T * (ttt[i+1] - ttt[1]))
         
         lower_bound = np.mean(value_testing)
+        point_estimate = np.mean(value)
         lower_bound_std = np.std(value_testing)
         
-        return lower_bound, lower_bound_std, regr
+        return lower_bound, lower_bound_std, regr, point_estimate
 
 class DeepDualPricer:
     """
@@ -272,7 +277,7 @@ class DeepDualPricer:
     """
     def __init__(self, N1, N, T, r, layers=3, nodes=64, activation_function='relu',
                  batch_normalization=False, regularizer=0.01, dropout=False,
-                 attention_layer=False, layer_normalization=False):
+                 attention_layer=False, layer_normalization=False,mode_dim = "1-dim"):
         """
         Parameters
         ----------
@@ -313,6 +318,7 @@ class DeepDualPricer:
         self.dropout = dropout
         self.attention_layer = attention_layer
         self.layer_normalization = layer_normalization
+        self.mode_dim = mode_dim
 
     def price(self, S_training_sig, Payoff_training, dW_training, S_testing_sig, Payoff_testing, dW_testing,
               M_val, batch=32, epochs=100, learning_rate=0.001):
@@ -355,37 +361,79 @@ class DeepDualPricer:
             regularizer=self.regularizer,
             dropout=self.dropout,
             attention_layer=self.attention_layer,
-            layer_normalization=self.layer_normalization
+            layer_normalization=self.layer_normalization,
+            mode_dim = self.mode_dim
         ).build_network_dual()
 
         optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
         model.compile(optimizer=optimizer)
 
-        early_stopping = EarlyStopping(monitor='val_loss', patience=5)
-        
-        model.fit(
+        early_stopping = EarlyStopping(monitor='val_loss', patience=10,restore_best_weights=True)
+        if self.mode_dim == "1-dim":
+            model.fit(
             [S_training_sig[:M_val, :, :], Payoff_training[:M_val, 1:self.N+1], dW_training[:M_val, :]],
             y=None,
             batch_size=batch,
             epochs=epochs,
             verbose=1,
-            validation_data=([S_training_sig[M_val:], Payoff_training[M_val:, 1:self.N+1], dW_training[M_val:, :]], None),
+            validation_data=([S_training_sig[M_val:,:,:], Payoff_training[M_val:, 1:self.N+1], dW_training[M_val:, :]], None),
             callbacks=[early_stopping]
-        )
+            )
+            res = model.predict([S_testing_sig, Payoff_testing[:, 1:self.N+1], dW_testing])
+            MG = rule_model.predict([S_testing_sig, dW_testing])
+            MG = np.concatenate((np.zeros((M2,1)), MG), axis=-1)
+            # Compute the upper bound and standard deviation
+            
+            upper_bound = np.mean(np.max(Payoff_testing[:, subindex2] - MG[:, subindex2], axis=1))
+            upper_bound_std = np.std(np.max(Payoff_testing[:, subindex2] - MG[:, subindex2], axis=1))
 
-        # Testing for fresh samples
-        res = model.predict([S_testing_sig, Payoff_testing[:, 1:self.N+1], dW_testing])
-        MG = rule_model.predict([S_testing_sig, dW_testing])
-        MG = np.concatenate((np.zeros((M2,1)), MG), axis=-1)
+            y0 = np.mean(res)
 
-        # Compute the upper bound and standard deviation
+            return y0, upper_bound, upper_bound_std, model, rule_model
+        elif self.mode_dim == "2-dim":
+            dW1_training = dW_training[:,:,0]
+            dW2_training = dW_training[:,:,1]
+            dW1_testing = dW_testing[:,:,0]
+            dW2_testing = dW_testing[:,:,1]
+            model.fit(
+            [S_training_sig[:M_val, :, :], Payoff_training[:M_val, 1:self.N+1], dW1_training[:M_val, :],dW2_training[:M_val, :]],
+            y=None,
+            batch_size=batch,
+            epochs=epochs,
+            verbose=1,
+            validation_data=([S_training_sig[M_val:,:,:], Payoff_training[M_val:, 1:self.N+1], dW1_training[M_val:, :],dW2_training[M_val:, :]], None),
+            callbacks=[early_stopping]
+            )
+            res = model.predict([S_testing_sig, Payoff_testing[:, 1:self.N+1], dW1_testing,dW2_testing])
+            MG = rule_model.predict([S_testing_sig, dW1_testing,dW2_testing])
+            MG = np.concatenate((np.zeros((M2,1)), MG), axis=-1)
+            model.summary()
+
+            # Compute the upper bound and standard deviation
+            
+            upper_bound = np.mean(np.max(Payoff_testing[:, subindex2] - MG[:, subindex2], axis=1))
+            upper_bound_std = np.std(np.max(Payoff_testing[:, subindex2] - MG[:, subindex2], axis=1))
+
+            y0 = np.mean(res)
+            return y0, upper_bound, upper_bound_std, model, rule_model
+
+class RReLU(tf.keras.layers.Layer):
+    def __init__(self, lower=0.125, upper=0.333, **kwargs):
+        super(RReLU, self).__init__(**kwargs)
+        self.lower = lower
+        self.upper = upper
+
+    def call(self, inputs, training=None):
+        if training:
+            # Randomly sample the negative slope from a uniform distribution
+            alpha = tf.random.uniform(shape=tf.shape(inputs), minval=self.lower, maxval=self.upper)
+            return tf.where(inputs >= 0, inputs, alpha * inputs)
+        else:
+            # Use the average slope during inference
+            alpha = (self.lower + self.upper) / 2.0
+            return tf.where(inputs >= 0, inputs, alpha * inputs)
         
-        upper_bound = np.mean(np.max(Payoff_testing[:, subindex2] - MG[:, subindex2], axis=1))
-        upper_bound_std = np.std(np.max(Payoff_testing[:, subindex2] - MG[:, subindex2], axis=1))
-
-        y0 = np.mean(res)
-
-        return y0, upper_bound, upper_bound_std, model, rule_model
+        
 
 class LongstaffSchwartzModel:
     """
@@ -422,11 +470,15 @@ class LongstaffSchwartzModel:
         if self.batch_normalization:
             model.add(layers.BatchNormalization(input_shape=(self.feature_dim,)))
         else:
+
             model.add(layers.Input(shape=(self.feature_dim,)))
 
         # Set activation function
         if self.activation_function == "LeakyRelu":
             activation = tf.keras.layers.LeakyReLU(negative_slope=0.3)
+        elif self.activation_function == "RRelu":
+            activation = RReLU()
+            
         else:
             activation = self.activation_function
 
@@ -489,6 +541,8 @@ class DeepMartingales(tf.keras.layers.Layer):
     def call(self, inputs, **kwargs):
         rule, dW = inputs
         return tf.cumsum(rule * dW, axis=1)
+    
+    
 
 class DualStoppingLoss(tf.keras.layers.Layer):
     def call(self, inputs):
@@ -500,15 +554,18 @@ class DualStoppingLoss(tf.keras.layers.Layer):
 
         return out
 
+
+
 class DualNetworkModel:
     def __init__(self, n, n2, I, q, d, activation_function='relu',
                  batch_normalization=False, regularizer=0.01, dropout=False,
-                 attention_layer=False, layer_normalization=False):
+                 attention_layer=False, layer_normalization=False,mode_dim = "1-dim"):
         self.n = n  # exercise dates
         self.n2 = n2  # discretization
         self.I = I  # number of layers
         self.q = q  # number of neurons
         self.d = d  # input dimension
+        self.mode_dim = mode_dim
         self.activation_function = activation_function
         self.batch_normalization = batch_normalization
         self.regularizer = regularizer
@@ -516,6 +573,49 @@ class DualNetworkModel:
         self.attention_layer = attention_layer
         self.layer_normalization = layer_normalization
         self.model, self.rule_model = self.build_network_dual()
+    
+
+
+   
+
+    def rnn_neural_network_dual(self):
+        # Choose the activation function
+        if self.activation_function == "LeakyRelu":
+            activation = tf.keras.layers.LeakyReLU(negative_slope=0.3)
+        else:
+            activation = self.activation_function
+
+        # Initialize the layers list
+        layers_list = []
+        
+        # Batch Normalization (if applicable)
+        if self.batch_normalization:
+            layers_list.append(layers.BatchNormalization())
+        
+        # First RNN layer (Simple RNN)
+        layers_list.append(layers.SimpleRNN(self.q, return_sequences=True, activation=activation,
+                                            kernel_regularizer=regularizers.l2(self.regularizer)))
+
+        # Additional RNN layers
+        for _ in range(self.I - 1):
+            if self.layer_normalization:
+                layers_list.append(layers.LayerNormalization(epsilon=1e-6))
+            if self.attention_layer:
+                layers_list.append(layers.MultiHeadAttention(num_heads=4,
+                                                            key_dim=max(1, self.q // 4),
+                                                            dropout=0.1))
+            layers_list.append(layers.SimpleRNN(self.q, return_sequences=True, activation=activation,
+                                                kernel_regularizer=regularizers.l2(self.regularizer)))
+            if self.layer_normalization:
+                layers_list.append(layers.LayerNormalization(epsilon=1e-6))
+            if self.dropout:
+                layers_list.append(layers.Dropout(0.5))
+
+        # Final output layer
+        layers_list.append(layers.Dense(1, activation=None))
+        layers_list.append(layers.Flatten())
+
+        return layers_list
 
     def dense_neural_network_dual(self):
         if self.activation_function == "LeakyRelu":
@@ -524,13 +624,14 @@ class DualNetworkModel:
             activation = self.activation_function
 
         layers_list = []
+        #layers_list.append(FeatureSelectionLayer())
         if self.batch_normalization:
             layers_list.append(layers.BatchNormalization())
-
+        num_attention_heads = 4
         layers_list.append(tf.keras.layers.Dense(self.q, activation=activation,
-                                                 kernel_regularizer=regularizers.l2(self.regularizer)))
-
-        num_attention_heads = 2
+                                                 kernel_regularizer=regularizers.l2(self.regularizer),kernel_initializer = tf.keras.initializers.GlorotUniform()))
+        #layers_list.append(SelfAttentionLayer(num_heads=4, key_dim=max(1, self.q // num_attention_heads), dropout=0.1))
+        
         for _ in range(self.I - 1):
             if self.layer_normalization:
                 layers_list.append(layers.LayerNormalization(epsilon=1e-6))
@@ -539,7 +640,7 @@ class DualNetworkModel:
                                                              key_dim=max(1, self.q // num_attention_heads),
                                                              dropout=0.1))
             layers_list.append(tf.keras.layers.Dense(self.q, activation=activation,
-                                                     kernel_regularizer=regularizers.l2(self.regularizer)))
+                                                     kernel_regularizer=regularizers.l2(self.regularizer),kernel_initializer = tf.keras.initializers.GlorotUniform()))
             if self.layer_normalization:
                 layers_list.append(layers.LayerNormalization(epsilon=1e-6))
             if self.dropout:
@@ -551,33 +652,72 @@ class DualNetworkModel:
         return layers_list
 
     def build_network_dual(self, initial_model=None):
+        print(f"Building network with parameters: I={self.I}, q={self.q}, d={self.d}, activation={self.activation_function}")
         input_logsig = tf.keras.Input(shape=(self.n2, self.d), name='sig')
         input_y = tf.keras.Input(shape=(self.n2-1,), name='Y')
-        input_BM = tf.keras.Input(shape=(self.n2-1,), name='dW')
+        
+        
+        if self.mode_dim == "1-dim":
+            dnn_layers = self.dense_neural_network_dual()
+            print(f"Number of layers created: {len(dnn_layers)}")
+            input_BM = tf.keras.Input(shape=(self.n2-1,), name='dW')
+            
+            try:
+                dnn_output = reduce(lambda x, f: f(x), [input_logsig] + dnn_layers)
+            except Exception as e:
+                print(f"Error in reduce function: {str(e)}")
+                print(f"input_logsig shape: {input_logsig.shape}")
+                print(f"dnn_layers: {dnn_layers}")
+                raise
+            rule_layer = DeepMartingales()([dnn_output[:, 0:self.n2-1], input_BM])
+            loss_layer = DualStoppingLoss()([
+                rule_layer[:, int((self.n2-1)/(self.n-1))-1:self.n2:int((self.n2-1)/(self.n-1))],
+                input_y[:, int((self.n2-1)/(self.n-1))-1:self.n2:int((self.n2-1)/(self.n-1))]
+            ])
 
-        dnn_layers = self.dense_neural_network_dual()
-        dnn_output = reduce(lambda x, f: f(x), [input_logsig] + dnn_layers)
+            model = tf.keras.Model([input_logsig, input_y, input_BM], loss_layer)
+            rule_model = tf.keras.Model([input_logsig, input_BM], rule_layer)
 
-        rule_layer = DeepMartingales()([dnn_output[:, 0:self.n2-1], input_BM])
-        loss_layer = DualStoppingLoss()([
-            rule_layer[:, int((self.n2-1)/(self.n-1))-1:self.n2:int((self.n2-1)/(self.n-1))],
-            input_y[:, int((self.n2-1)/(self.n-1))-1:self.n2:int((self.n2-1)/(self.n-1))]
-        ])
+            if initial_model is not None:
+                model.set_weights(initial_model.get_weights())
 
-        model = tf.keras.Model([input_logsig, input_y, input_BM], loss_layer)
-        rule_model = tf.keras.Model([input_logsig, input_BM], rule_layer)
+            return model, rule_model
+        elif self.mode_dim == "2-dim":
+            dnn_layers_1 = self.dense_neural_network_dual()
+            dnn_layers_2 = self.dense_neural_network_dual()
+            print(f"Number of layers created: {len(dnn_layers_1)} for each network")
+            input_BM_1 = tf.keras.Input(shape=(self.n2-1,), name='dW1')
+            input_BM_2 = tf.keras.Input(shape=(self.n2-1,), name='dW2')
+            try:
+                dnn_output_1 = reduce(lambda x, f: f(x), [input_logsig] + dnn_layers_1)
+                dnn_output_2 = reduce(lambda x, f: f(x), [input_logsig] + dnn_layers_2)
+            except Exception as e:
+                print(f"Error in reduce function: {str(e)}")
+                print(f"input_logsig shape: {input_logsig.shape}")
+                print(f"dnn_layers: {dnn_layers}")
+                raise
+            rule_layer_1 = DeepMartingales()([dnn_output_1[:, 0:self.n2-1], input_BM_1])
+            rule_layer_2 = DeepMartingales()([dnn_output_2[:, 0:self.n2-1], input_BM_2])
+            loss_layer = DualStoppingLoss()([
+                rule_layer_1[:, int((self.n2-1)/(self.n-1))-1:self.n2:int((self.n2-1)/(self.n-1))]+rule_layer_2[:, int((self.n2-1)/(self.n-1))-1:self.n2:int((self.n2-1)/(self.n-1))],
+                input_y[:, int((self.n2-1)/(self.n-1))-1:self.n2:int((self.n2-1)/(self.n-1))]
+            ])
 
-        if initial_model is not None:
-            model.set_weights(initial_model.get_weights())
+            model = tf.keras.Model([input_logsig, input_y, input_BM_1,input_BM_2], loss_layer)
+            rule_model = tf.keras.Model([input_logsig, input_BM_1,input_BM_2], rule_layer_1+rule_layer_2)
+            if initial_model is not None:
+                model.set_weights(initial_model.get_weights())
 
-        return model, rule_model
+            return model, rule_model
+
+
+        
 
     def compile(self, optimizer='adam', loss='mse', metrics=['mae']):
         self.model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
-    def fit(self, X, y, epochs=100, batch_size=32, validation_split=0.2, verbose=1):
-        return self.model.fit(X, y, epochs=epochs, batch_size=batch_size,
-                              validation_split=validation_split, verbose=verbose)
+    def fit(self, X, y, epochs=100, batch_size=32, verbose=1):
+        return self.model.fit(X, y, epochs=epochs, batch_size=batch_size, verbose=verbose)
 
     def predict(self, X):
         return self.rule_model.predict(X)
